d4cf90c6677b25206104fa20226cbec2e50553bd
Andrey Dergachev <60715134+d84and@users.noreply.github.com>
2023-07-15 16:55:41 +0300

    Add files via upload

---
+++ Лабораторная работа 1v.2/task_3.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+seredina = len(list_players) // 2
+first_team = list_players[:seredina]
+second_team = list_players[seredina:]
+print(first_team)
+print(second_team)
+# TODO Разделите участников на две команды
