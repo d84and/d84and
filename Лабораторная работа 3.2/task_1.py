a0d5e4aafc75ea98b43be0345d7d264e862536cd
Andrey Dergachev <60715134+d84and@users.noreply.github.com>
2023-08-04 00:13:42 +0300

    Add files via upload

--- Лабораторная работа 3.2/task_1.py
+++ Лабораторная работа 3.2/task_1.py
@@ -1,23 +1,14 @@
 # TODO Напишите функцию find_common_participants
-def find_common_participants(stroka_1, stroka_2, razdelitel="!"):
-    common = [x for x in stroka_1 if x in stroka_2]
+def find_common_participants(stroka_1,stroka_2,razdelitel="|"):
+    familii_1 = stroka_1.split(razdelitel)
+    familii_2 = stroka_2.split(razdelitel)
+    common = {x for x in familii_1 if x in familii_2}
     return common
-
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
-new_common = participants_first_group.split("|")
-new_common_1 = participants_second_group.split("|")
-print(new_common)
-print(new_common_1)
-common_1 = [j for j in new_common if j in new_common_1]
-result = find_common_participants(participants_first_group, participants_second_group)
+result = find_common_participants(participants_first_group, participants_second_group, razdelitel="|")
 print(result)
 
-new_common = participants_first_group.split("|")
-new_common_1 = participants_second_group.split("|")
-#print(new_common)
-#print(new_common_1)
-common_1 = [j for j in new_common if j in new_common_1]
-print(common_1)
+
 # TODO Провеьте работу функции с разделителем отличным от запятой
